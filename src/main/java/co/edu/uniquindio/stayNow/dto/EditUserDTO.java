9a8c6b362c8fd76fb8ad327db5531cca7a1fb326
juandr <juand.ramirezc@uqvirtual.edu.co>
2025-09-15 18:45:59 -0500

    Debugging

--- src/main/java/co/edu/uniquindio/stayNow/dto/EditUserDTO.java
+++ src/main/java/co/edu/uniquindio/stayNow/dto/EditUserDTO.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.stayNow.dto;
 
 import co.edu.uniquindio.stayNow.model.enums.Role;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
@@ -9,7 +10,9 @@ import java.time.LocalDate;
 
 public record EditUserDTO(
         @NotBlank @Length(max = 100) String name,
+        @Email@NotNull String email,
         @Length(max = 10) String phone,
+        @NotNull @NotBlank @Length(min = 8) String password,
         @Length(max = 300) String photoUrl,
         @NotNull @Past LocalDate dateBirth,
         @NotNull Role role
--- src/main/java/co/edu/uniquindio/stayNow/dto/ValidationDTO.java
+++ src/main/java/co/edu/uniquindio/stayNow/dto/ValidationDTO.java
@@ -1,4 +1,13 @@
 package co.edu.uniquindio.stayNow.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class ValidationDTO {
+    String field;
+    String message;
+
 }
